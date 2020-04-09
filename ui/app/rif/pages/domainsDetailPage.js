f61cf7a10961ecc414ec0db4a1ef81e53f8288d7
Rodrigo <rorolopetegui@gmail.com>
2020-04-09 15:41:24 -0300

    Domain detail Page updated with resolver section

--- ui/app/rif/pages/domainsDetailPage.js
+++ ui/app/rif/pages/domainsDetailPage.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle, faArchive, faBolt, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle, faArchive, faBolt, faChevronLeft, faCoins, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faBitcoin, faEthereum } from '@fortawesome/free-brands-svg-icons'
 import rifActions from '../../rif/actions'
 
 class DomainsDetailScreen extends Component {
-  
+	state = {
+		resolvers: this.props.resolvers,
+		selectedResolverIndex: 0,
+	}
 	navigateTo (url) {
 		global.platform.openWindow({ url })
   	}
-
+	admitedCoins = (coin) => {
+		let value = faCoins
+		switch (coin.toLowerCase()){
+			case 'bitcoin':
+				value = faBitcoin
+			break
+			case 'ethereum':
+				value = faEthereum
+			break
+		}
+		return value
+	}
 	render () {
 		const { status, domainName, address, content, expirationDate, autoRenew, ownerAddress, isOwner, isLuminoNode, isRifStorage } = this.props
 		return (
@@ -18,24 +33,59 @@ class DomainsDetailScreen extends Component {
 			<FontAwesomeIcon icon={faChevronLeft} className={'rif-back-button'} onClick={() => this.props.goBack()}/>
 			{status === 'active' && 
 			<div>
-				<div id='headerName' className={'domainName'}>
+				<div id='headerName' className={'domain-name'}>
 					<span>{domainName}</span>
 					{isOwner &&
-						<FontAwesomeIcon icon={faCheckCircle} color="#000080" className={'domainIcon'}/>
+						<FontAwesomeIcon icon={faCheckCircle} color="#000080" className={'domain-icon'}/>
 					}
 					{isLuminoNode &&
-						<FontAwesomeIcon icon={faBolt} color="#508871" className={'domainIcon'}/>
+						<FontAwesomeIcon icon={faBolt} color="#508871" className={'domain-icon'}/>
 					}
 					{isRifStorage &&
-						<FontAwesomeIcon icon={faArchive} color="#AD3232" className={'domainIcon'}/>
+						<FontAwesomeIcon icon={faArchive} color="#AD3232" className={'domain-icon'}/>
 					}
 				</div>
-				<div id='bodyDescription' className={'domainDescription'}>
-					<div>Address: <span>{address}</span></div>
-					<div>Content: <span>{content}</span></div>
-					<div>Expires on: <span>{expirationDate}</span></div>
-					<div>Auto renew: <span>{autoRenew}</span></div>
-					<div>Owner: <span>{ownerAddress}</span></div>
+				<div id='domainDetailBody' className={'domain-detail-body'}>
+					<div id='bodyDescription' className={'domain-description'}>
+						<div>Address: <span>{address}</span></div>
+						<div>Content: <span>{content}</span></div>
+						<div>Expires on: <span>{expirationDate}</span></div>
+						<div>Auto renew: <span>{autoRenew}</span></div>
+						<div>Owner: <span>{ownerAddress}</span></div>
+					</div>
+					{isOwner &&
+						<div id='resolversBody' className={'resolvers-body'}>
+							<div className='resolver-body-top'>
+								<div id='selectResolver' className={'custom-select'}>
+									<select id='comboResolvers' onChange={(value) => this.setState({selectedResolverIndex:value.target.selectedIndex})}>
+										{this.state.resolvers.map((resolver, index) => {
+												return <option key={index} value={resolver.name}>{resolver.name}</option>
+											})
+										}								
+									</select>
+								</div>
+								{
+									//TODO
+									//This button will need to be moved to a component later
+								}
+								<div id='buttonNew' className={'domain-detail-new-button'}>
+									<div id='buttonBody'>
+										<FontAwesomeIcon icon={faPlusCircle} color="#235BAC" className={'domain-icon centerY'}/>
+										<span className={'center'}>NEW</span>
+									</div>
+								</div>
+							</div>
+							<div id='resolverNetworksBody' className={'resolver-network'}>
+								{this.state.resolvers[this.state.selectedResolverIndex].network.map((network, index) => {
+									return <div key={index} className={'resolver-network-description'}>
+											<FontAwesomeIcon icon={this.admitedCoins(network.networkIcon)} color="#000080" className={'domain-icon'}/>
+											<span>{network.networkName}</span>
+											<span className={'resolver-network-description-address'}>{network.address}</span>
+										</div>
+								})}
+							</div>
+						</div>
+					}
 				</div>
 			</div>
 			}
@@ -78,6 +128,7 @@ function mapStateToProps (state) {
 		isOwner: state.metamask.selectedAddress.toLowerCase() === data.ownerAddress.toLowerCase(),
 		isLuminoNode: data.isLuminoNode,
 		isRifStorage: data.isRifStorage,
+		resolvers: data.resolvers,
 	}
 }
 
--- ui/app/rif/pages/domainsPage.js
+++ ui/app/rif/pages/domainsPage.js
@@ -18,6 +18,33 @@ const mockDomains = [
 		ownerAddress: '0x5F4df703Da966E12d3068E1aCbe930f2E363c732',
 		isLuminoNode: true,
 		isRifStorage: true,
+		resolvers: [
+			{
+				name: "MultiCrypto",
+				network: [
+					{
+						networkName: 'Bitcoin',
+						networkIcon: 'bitcoin',
+						address: '0x00000000000000',
+					},
+					{
+						networkName: 'RSK',
+						networkIcon: 'rsk',
+						address: '0x00000000000000',
+					},
+				],
+			},
+			{
+				name: "AnotherResolver",
+				network: [
+					{
+						networkName: 'Ethereum',
+						networkIcon: 'ethereum',
+						address: '0x00000000000000',
+					},
+				],
+			},
+		],
 	},
 	{
 		domain: 'blockchain.rsk',
@@ -59,9 +86,36 @@ const mockDomains = [
 		status: 'active',
 		address: '0x123456789',
 		content: 'abcdefg1234abcd1234aaaabbbbddddd',
-		ownerAddress: '0x123456789',
+		ownerAddress: '0x7b2BcC246d0120C35B2CC7f52ece3E58df592C77',
 		isLuminoNode: true,
 		isRifStorage: true,
+		resolvers: [
+			{
+				name: "MultiCrypto",
+				network: [
+					{
+						networkName: 'Bitcoin',
+						networkIcon: 'bitcoin',
+						address: '0x00000000000000',
+					},
+					{
+						networkName: 'RSK',
+						networkIcon: 'rsk',
+						address: '0x00000000000000',
+					},
+				],
+			},
+			{
+				name: "AnotherResolver",
+				network: [
+					{
+						networkName: 'Ethereum',
+						networkIcon: 'ethereum',
+						address: '0x00000000000000',
+					},
+				],
+			},
+		],
 	},
 	{
 		domain: 'doe.rsk',
@@ -70,9 +124,36 @@ const mockDomains = [
 		status: 'active',
 		address: '0x123456789',
 		content: 'abcdefg1234abcd1234aaaabbbbddddd',
-		ownerAddress: '0x123456789',
+		ownerAddress: '0x7b2BcC246d0120C35B2CC7f52ece3E58df592C77',
 		isLuminoNode: true,
 		isRifStorage: true,
+		resolvers: [
+			{
+				name: "MultiCrypto",
+				network: [
+					{
+						networkName: 'Bitcoin',
+						networkIcon: 'bitcoin',
+						address: '0x00000000000000',
+					},
+					{
+						networkName: 'RSK',
+						networkIcon: 'rsk',
+						address: '0x00000000000000',
+					},
+				],
+			},
+			{
+				name: "AnotherResolver",
+				network: [
+					{
+						networkName: 'Ethereum',
+						networkIcon: 'ethereum',
+						address: '0x00000000000000',
+					},
+				],
+			},
+		],
 	},
 ]
 
