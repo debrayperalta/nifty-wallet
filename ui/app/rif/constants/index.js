243a597c107dd260b6abcc7be8c4ffdebd6ad182
Rodrigo <rorolopetegui@gmail.com>
2020-04-13 10:00:18 -0300

    Changed utils.js, and constants

--- ui/app/rif/constants/index.js
+++ ui/app/rif/constants/index.js
@@ -1,13 +1,10 @@
 //import icons for each token
 import { faBitcoin, faEthereum } from '@fortawesome/free-brands-svg-icons'
-//Default icon
-import { faCoins } from '@fortawesome/free-solid-svg-icons'
 /**
  * Add the icon to the array with this structure
  *   name: icon,
 */
 export const cryptos = {
-    default: faCoins,
     bitcoin: faBitcoin,
     ethereum: faEthereum,
 }
--- ui/app/rif/utils/utils.js
+++ ui/app/rif/utils/utils.js
@@ -1,9 +1,12 @@
 //Constant names, if you want to add a new token (icon), just go to constant.js and add one to the array, then add it to getNameTokenForIcon
 import { cryptos } from '../constants'
 
+//Default icon
+import { faCoins } from '@fortawesome/free-solid-svg-icons'
+
 var getIconForToken = function(tokenName) {
     let cryto = cryptos[tokenName]
-    return cryto ? cryto : cryptos.default
+    return cryto ? cryto : faCoins
 }
 
 export {
