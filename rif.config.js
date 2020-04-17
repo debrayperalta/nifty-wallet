23635e4384605cd7392bfaf73c07a2ec02dd5bdc
Jonathan <jonaf2103@gmail.com>
2020-04-17 10:52:34 -0300

    RIFEX-61 Integrating RNS
    * Cleaning config file

--- rif.config.js
+++ rif.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   rns: {
     contracts: {
-      rns: '0x03F23ae1917722d5A27a2Ea0Bcc98725a2a2a49a',
-      registrar: '0xfD1dda8C3BC734Bc1C8e71F69F25BFBEe9cE9535',
-      reverseRegistrar: '0x8901a2Bbf639bFD21A97004BA4D7aE2BD00B8DA8',
-      publicResolver: '0x1eD614cd3443EFd9c70F04b6d777aed947A4b0c4',
-      nameResolver: '0x0e19674ebc2c2B6Df3e7a1417c49b50235c61924',
-      multiChainResolver: '0x5159345aaB821172e795d56274D0f5FDFdC6aBD9',
-      rif: '0xdac5481925A298B95Bf5b54c35b68FC6fc2eF423',
-      fifsRegistrar: '0x8921BF2f074b5470c02Cc7473F17282576111591',
-      fifsAddrRegistrar: '0xc53A82b9B7c9af4801c7d8EA531719E7657aFF3C',
-      rskOwner: '0xA66939ac57893C2E65425a5D66099Bc20C76D4CD',
-      renewer: '0x23EF9610F53092A66bf224862BfD45216d9f3ea2',
-      stringResolver: '0x20804b7317D2F4d0d2123f30c2D3A6B0E33DfB37'
+      rns: '',
+      registrar: '',
+      reverseRegistrar: '',
+      publicResolver: '',
+      nameResolver: '',
+      multiChainResolver: '',
+      rif: '',
+      fifsRegistrar: '',
+      fifsAddrRegistrar: '',
+      rskOwner: '',
+      renewer: '',
+      stringResolver: '',
     },
   },
 }
