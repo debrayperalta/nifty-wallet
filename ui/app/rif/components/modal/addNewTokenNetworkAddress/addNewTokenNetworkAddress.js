d210711c9a89407fefa80fd56c4fb8154f677727
Rodrigo <rorolopetegui@gmail.com>
2020-04-17 10:13:58 -0300

    removed import unused

--- ui/app/rif/components/modal/addNewTokenNetworkAddress/addNewTokenNetworkAddress.js
+++ ui/app/rif/components/modal/addNewTokenNetworkAddress/addNewTokenNetworkAddress.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { cryptos } from '../../../constants'
 import Select from 'react-select'
 
 class AddNewTokenNetworkAddress extends Component {
