1a824311b3f22166b8922660f6b0f6cd9eb0bbff
Rodrigo <rorolopetegui@gmail.com>
2020-05-22 09:46:42 -0300

    Created page for token detail

---
+++ ui/app/rif/pages/lumino/tokenDetailPage/tokenDetailPage.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {pageNames} from '../../index';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import rifActions from '../../../actions';
+import PropTypes from 'prop-types';
+import { tokenIcons, brandConnections, DEFAULT_ICON } from '../../../constants/icons';
+import {Menu} from '../../../components';
+
+const PATH_TO_RIF_IMAGES = '/images/rif/';
+
+class LuminoTokenDetailPage extends Component {
+  static propTypes = {
+    token: PropTypes.object,
+    channels: PropTypes.array,
+    showThis: PropTypes.func,
+    getChannels: PropTypes.func,
+  }
+
+  render () {
+    const { token } = this.props;
+    if (token) {
+      return (<div className={'body'}>
+        Token
+        <Menu />
+      </div>)
+    } else {
+      return (<div>Loading tokens...</div>);
+    }
+  }
+}
+
+function mapStateToProps (state) {
+  const params = state.appState.currentView.params;
+  return {
+    dispatch: state.dispatch,
+    ...params,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    showThis: (params) => dispatch(rifActions.navigateTo(pageNames.rns.luminoTokensPage, params)),
+    getChannels: () => dispatch(rifActions.getChannels()),
+  }
+}
+
+module.exports = connect(mapStateToProps, mapDispatchToProps)(LuminoTokenDetailPage)
