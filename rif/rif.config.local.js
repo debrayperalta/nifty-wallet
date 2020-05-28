dac9fc17f4c19c15785fe5b238b8797269cdcfba
Jonathan <jonaf2103@gmail.com>
2020-05-28 18:35:16 -0300

    RIFEX-88 Adding multiple environment support for configuration
    * Cleaning local file

--- rif/rif.config.local.js
+++ rif/rif.config.local.js
@@ -1,26 +1,26 @@
 module.exports = {
   lumino: {
     hub: {
-      endpoint: 'http://127.0.0.1:5001/api/v1',
+      endpoint: '',
     },
     explorer: {
-      endpoint: 'http://localhost:8080/api/v1',
+      endpoint: '',
     },
   },
   rns: {
     contracts: {
-      rns: '0x61026975E4dCc28bF56fE1Da18c7482922501C08',
-      registrar: '0x7Aa98966d3FE1BbDd1fbb2F5Ef761462Db6f642e',
-      reverseRegistrar: '0x7b089cfE50C1a5fE5b0DA352348A43bBa81ADdd4',
-      publicResolver: '0xD7898483B4fD49CFc847C63E4825D3900EBFcB55',
-      nameResolver: '0x4F7b5156094e8cFcda28821dC05c5d2Cea58448f',
-      multiChainResolver: '0x57153C4a4b3cE7D627D67FAc081733F3735fCd57',
-      rif: '0xF76e6b7db78ac46BBDC7F9013CE4c761B7E7024e',
-      fifsRegistrar: '0x765fc394499e8EdD99266fBf88f3CAfBA407d799',
-      fifsAddrRegistrar: '0xa5f9982F1b29Ea52aa0e44aC1a299d04A08AF4C5',
-      rskOwner: '0x67Da7F80A6842E9E6Cf0378227bCE5693156f659',
-      renewer: '0xA8eE9964B79f1D88E8DDC5A2821e6be0d31D0eb0',
-      stringResolver: '0x2820c09211a077A9d54E6fB9f5fd1e3faBe79e07',
+      rns: '',
+      registrar: '',
+      reverseRegistrar: '',
+      publicResolver: '',
+      nameResolver: '',
+      multiChainResolver: '',
+      rif: '',
+      fifsRegistrar: '',
+      fifsAddrRegistrar: '',
+      rskOwner: '',
+      renewer: '',
+      stringResolver: '',
     },
   },
 }
