8484176f31933344e52b2a3c0ea2698bb7537072
Debray Peralta <debray.peralta@gmail.com>
2020-06-08 14:26:52 -0300

    Refactor Item Functional Component to class

--- ui/app/rif/components/item-with-actions/index.js
+++ ui/app/rif/components/item-with-actions/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { faPen, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,67 +9,71 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
  * By default the icons are disabled, and must be explicitly rendered with the enable prop
  * By default the edit click will render the children that are passed to it
  */
-const ItemWithActions = (props) => {
-  const [showEditChildren, setshowEditChildren] = useState(false);
-  const {
-    enableEdit = false,
-    enableDelete = false,
-    text = '',
-    onEditClick = null,
-    onDeleteClick = () => {},
-    leftIcon = null,
-    children,
-  } = props;
+class ItemWithActions extends Component {
 
+  static propTypes = {
+    enableEdit: PropTypes.bool,
+    enableDelete: PropTypes.bool,
+    text: PropTypes.string.isRequired,
+    onEditClick: PropTypes.func,
+    onDeleteClick: PropTypes.func,
+    leftIcon: PropTypes.shape({icon: PropTypes.string, color: PropTypes.string }),
+    leftContent: PropTypes.element,
+    children: PropTypes.element,
+  }
 
-  const toggleShowEditChildren = () => setshowEditChildren(!showEditChildren);
+  constructor (props) {
+    super(props);
+    this.state = {
+      showEditChildren: false,
+    }
+  }
 
-  const editClick = () => {
-    if (onEditClick) return onEditClick();
-    return toggleShowEditChildren();
-  };
+  toggleShowEditChildren = () => this.setState({showEditChildren: !this.state.showEditChildren});
 
-  return (
-    <div>
-      {leftIcon && (
-        <div>
-          <FontAwesomeIcon
-            icon={leftIcon.icon}
-            color={leftIcon.color}
-            className={''}
-          />
-        </div>
-      )}
-      <div>
-        <p>{text}</p>
-      </div>
-      <div className={''}>
-        {enableEdit && (
-          <FontAwesomeIcon icon={faPen} className={''} onClick={editClick} />
-        )}
-        {enableDelete && (
-          <FontAwesomeIcon
-            icon={faTimes}
-            className={''}
-            onClick={onDeleteClick}
-          />
-        )}
-      </div>
-      {showEditChildren && children && (
-        children
-      )}
-    </div>
-  );
-};
+   onEditClick = () => {
+     const {onEditClick} = this.props;
+     if (onEditClick) onEditClick();
+     this.toggleShowEditChildren();
+    };
 
-ItemWithActions.propTypes = {
-  enableEdit: PropTypes.bool,
-  enableDelete: PropTypes.bool,
-  text: PropTypes.string.isRequired,
-  onEditClick: PropTypes.func,
-  onDeleteClick: PropTypes.func,
-  leftIcon: PropTypes.shape({icon: PropTypes.string, color: PropTypes.string }),
-  children: PropTypes.element,
-};
+   render = () => {
+      const {leftIcon, leftContent, text, enableEdit, enableDelete, children, onDeleteClick} = this.props;
+      const {showEditChildren } = this.state;
+      return (
+       <div>
+         {leftIcon && (
+           <div>
+             <FontAwesomeIcon
+               icon={leftIcon.icon}
+               color={leftIcon.color}
+               className={''}
+             />
+           </div>
+         )}
+         {leftContent && <div>{leftContent}</div>}
+         <div>
+           <p>{text}</p>
+         </div>
+         <div className={''}>
+           {enableEdit && (
+             <FontAwesomeIcon icon={faPen} className={''} onClick={this.onEditClick} />
+           )}
+           {enableDelete && (
+             <FontAwesomeIcon
+               icon={faTimes}
+               className={''}
+               onClick={onDeleteClick}
+             />
+           )}
+         </div>
+         {showEditChildren && children && (
+           children
+         )}
+       </div>
+     );
+   }
+
+}
 
 export default ItemWithActions;
