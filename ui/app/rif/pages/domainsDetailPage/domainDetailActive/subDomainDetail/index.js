4e965cb0de0bff9b81cb2e8ed3269df9ad128d48
Debray Peralta <debray.peralta@gmail.com>
2020-06-09 12:50:30 -0300

    Get subdomain details when mounting component

--- ui/app/rif/pages/domainsDetailPage/domainDetailActive/subDomainDetail/index.js
+++ ui/app/rif/pages/domainsDetailPage/domainDetailActive/subDomainDetail/index.js
@@ -3,30 +3,53 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {ChainAddresses, LuminoChannels} from '../../../../components';
 import TransferTable from '../../../../components/transferTable';
+import rifActions from '../../../../actions';
 
 class SubDomainDetail extends Component {
 
   static propTypes = {
     domainName: PropTypes.string.isRequired,
+    getSubdomains: PropTypes.func,
     name: PropTypes.string.isRequired,
   }
 
+  constructor (props) {
+    super(props);
+    const {name, domainName} = props;
+    this.state = {
+      subDomainFullName: `${name}.${domainName}`,
+      controllerAddress: '',
+    }
+  }
+
+  componentDidMount = async () => {
+    const {getSubdomains, domainName, name} = this.props
+    const allSubdomains = await getSubdomains(domainName);
+    const subdomain = allSubdomains.find(s => s.name === name);
+    if (subdomain) this.setState({controllerAddress: subdomain.ownerAddress})
+  }
+
   render = () => {
-    const {domainName, name} = this.props;
-    const mockAddress = '0x123456789012345'
-    const subDomainFullName = `${name}.${domainName}`
+    const {subDomainFullName, controllerAddress} = this.state;
     return <div>
       <h3>{subDomainFullName}</h3>
       <div>
         <ChainAddresses domainName={subDomainFullName}/>
       </div>
-      <div>
-        <TransferTable controllerAddress={mockAddress}/>
+      {controllerAddress && <div>
+        <TransferTable controllerAddress={controllerAddress}/>
       </div>
+      }
       <div>
         <LuminoChannels/>
       </div>
     </div>
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    getSubdomains: (domainName) => dispatch(rifActions.getSubdomains(domainName)),
   }
 }
 
@@ -37,4 +60,4 @@ function mapStateToProps (state) {
   }
 }
 
-export default connect(mapStateToProps, null)(SubDomainDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(SubDomainDetail);
