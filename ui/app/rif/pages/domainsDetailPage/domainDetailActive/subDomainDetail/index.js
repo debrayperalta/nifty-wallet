bc25046a098ef4cdf399f83fdc7c20ee8be05367
Debray Peralta <debray.peralta@gmail.com>
2020-06-09 10:09:05 -0300

    Connect subdomain detail to redux and use props

--- ui/app/rif/pages/domainsDetailPage/domainDetailActive/subDomainDetail/index.js
+++ ui/app/rif/pages/domainsDetailPage/domainDetailActive/subDomainDetail/index.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 
 class SubDomainDetail extends Component {
 
+  static propTypes = {
+    domainName: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }
+
   render = () => {
-    console.warn(this.props);
-    return <div>Hello world!</div>
+    const {domainName, name} = this.props;
+    return <div><h3>{name}.{domainName}</h3></div>
   }
 }
 
-export default SubDomainDetail;
+function mapStateToProps (state) {
+  const params = state.appState.currentView.params;
+  return {
+    ...params,
+  }
+}
+
+export default connect(mapStateToProps, null)(SubDomainDetail);
